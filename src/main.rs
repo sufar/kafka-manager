9c4ac30b4b6719f65d397dfa16399321eaed3c37
sufar <rust@88.com>
2026-03-11 11:37:52 +0000

    topic查询优化

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "kafka_manager=debug,tower_http=debug".into()),
+                .unwrap_or_else(|_| "kafka_manager_api=debug,tower_http=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
