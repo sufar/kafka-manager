3965ca5b6a565a8968e21615b28da7cc951ce177
sufar <rust@88.com>
2026-06-06 09:32:01 +0000

    打开应用优化

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2515,6 +2515,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet, get_app_version, check_for_updates, install_update, get_app_logs, clear_app_logs, get_download_status, clear_download_status, set_auto_launch, get_auto_launch, set_system_tray, is_windows, share_current_version, open_url])
         .setup(|app| {
             // 启动时清理，只保留今天的日志
