951d9248b6a092959fb14e90e5c3f9e9db06feec
sufar <rust@88.com>
2026-06-08 04:02:42 +0000

    消息界面优化

--- ui/src/components/MessageQueryTool.vue
+++ ui/src/components/MessageQueryTool.vue
@@ -115,7 +115,7 @@
 
     <!-- 状态栏 -->
     <div class="status-bar flex items-center justify-between px-3 py-1 text-xs border-b border-base-300 bg-base-200/50">
-      <div class="flex items-center gap-4">
+      <div class="flex items-center gap-2">
         <span v-if="selectedTopic" class="text-base-content/70 flex items-center gap-1">
           {{ t.messages.topicLabel }}:
           <span class="font-mono font-bold text-primary relative group cursor-help">
@@ -142,37 +142,41 @@
         </span>
         <span v-if="messages.length > 0" class="text-base-content/70">
           {{ t.messages.totalMessages }} <span class="font-mono font-bold text-success">{{ messages.length.toLocaleString() }}</span> {{ t.messages.messages }}
-          <span class="tooltip tooltip-right ml-1" :data-tip="t.messages.exportTip">
-            <button class="btn btn-ghost btn-xs btn-circle" @click="exportMessages" :disabled="messages.length === 0" :title="t.messages.exportMessages">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+        </span>
+        <div class="flex items-center gap-0.5">
+          <!-- 导出提示 -->
+          <span v-if="messages.length > 0" class="tooltip tooltip-right" :data-tip="t.messages.exportTip">
+            <button class="btn btn-ghost btn-xs btn-circle" disabled>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
               </svg>
             </button>
           </span>
-          <button class="btn btn-ghost btn-xs ml-1" :disabled="messages.length === 0" @click="exportMessages" :title="t.messages.exportMessages" data-tour="messages-export">
+          <!-- 导出消息 -->
+          <button class="btn btn-ghost btn-xs" :disabled="messages.length === 0" @click="exportMessages" :title="t.messages.exportMessages" data-tour="messages-export">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
             </svg>
           </button>
-        </span>
-        <!-- 发送历史 -->
-        <button class="btn btn-ghost btn-xs" :disabled="!selectedCluster || !selectedTopic" :title="t.sentMessageHistory?.title || '发送历史'" @click="toggleHistory" data-tour="messages-history">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3.5 h-3.5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0Z" />
-          </svg>
-        </button>
-        <!-- 消费者组 -->
-        <button class="btn btn-ghost btn-xs" :disabled="!selectedCluster || !selectedTopic" :title="t.topicConsumerGroups?.title || '消费者组'" @click="viewTopicConsumerGroups" data-tour="messages-consumer-groups">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3.5 h-3.5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.75a.75.75 0 0 0 .75-.75c0-.178-.012-.355-.036-.528A9.75 9.75 0 0 0 12 3.75c-1.324 0-2.595.274-3.75.772V18h9.75ZM12 2.25c-2.485 0-4.856.488-7.062 1.38a.75.75 0 0 0-.447.932l.958 3.758a.75.75 0 0 0 .973.536 8.25 8.25 0 0 1 10.572 0 .75.75 0 0 0 .973-.536l.958-3.758a.75.75 0 0 0-.447-.932A18.25 18.25 0 0 0 12 2.25Z" />
-          </svg>
-        </button>
-        <!-- 删除主题 -->
-        <button class="btn btn-ghost btn-xs text-error" :disabled="!selectedCluster || !selectedTopic" :title="t.messages?.deleteTopic || '删除主题'" @click="handleDeleteTopic" data-tour="messages-delete-topic">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3.5 h-3.5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-          </svg>
-        </button>
+          <!-- 发送历史 -->
+          <button class="btn btn-ghost btn-xs" :disabled="!selectedCluster || !selectedTopic" :title="t.sentMessageHistory?.title || '发送历史'" @click="toggleHistory" data-tour="messages-history">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0Z" />
+            </svg>
+          </button>
+          <!-- 消费者组 -->
+          <button class="btn btn-ghost btn-xs" :disabled="!selectedCluster || !selectedTopic" :title="t.topicConsumerGroups?.title || '消费者组'" @click="viewTopicConsumerGroups" data-tour="messages-consumer-groups">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.75a.75.75 0 0 0 .75-.75c0-.178-.012-.355-.036-.528A9.75 9.75 0 0 0 12 3.75c-1.324 0-2.595.274-3.75.772V18h9.75ZM12 2.25c-2.485 0-4.856.488-7.062 1.38a.75.75 0 0 0-.447.932l.958 3.758a.75.75 0 0 0 .973.536 8.25 8.25 0 0 1 10.572 0 .75.75 0 0 0 .973-.536l.958-3.758a.75.75 0 0 0-.447-.932A18.25 18.25 0 0 0 12 2.25Z" />
+            </svg>
+          </button>
+          <!-- 删除主题 -->
+          <button class="btn btn-ghost btn-xs text-error" :disabled="!selectedCluster || !selectedTopic" :title="t.messages?.deleteTopic || '删除主题'" @click="handleDeleteTopic" data-tour="messages-delete-topic">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>
+          </button>
+        </div>
         <span v-if="error" class="text-error">{{ error }}</span>
       </div>
       <!-- 流式进度条 -->
