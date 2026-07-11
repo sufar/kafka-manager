7bc9aad6b4d7383634b26bbfb8c10d29ec4a1c81
sufar <rust@88.com>
2026-07-11 07:44:26 +0000

    消息界面支持搜索键或值

--- ui/src/components/MessageQueryTool.vue
+++ ui/src/components/MessageQueryTool.vue
@@ -40,16 +40,13 @@
 
       <!-- 搜索 -->
       <div class="flex-1 min-w-[120px] relative" data-tour="messages-search">
-        <div class="flex items-center input input-bordered input-sm w-full">
-          <!-- 搜索范围选择器 -->
-          <select v-model="searchIn" class="select select-ghost select-xs w-auto p-0 h-auto text-xs bg-transparent border-none focus:outline-none cursor-pointer">
-            <option value="all">{{ t.messages.searchInAll }}</option>
-            <option value="key">{{ t.messages.searchInKey }}</option>
-            <option value="value">{{ t.messages.searchInValue }}</option>
-          </select>
-          <!-- 搜索输入框 -->
-          <input v-model="searchKeyword" type="text" class="input input-ghost input-xs flex-1 w-full h-auto p-0 bg-transparent border-none focus:outline-none" :placeholder="t.messages.searchPlaceholder" @keyup.enter="queryMessages" />
-        </div>
+        <input v-model="searchKeyword" type="text" class="input input-bordered input-sm w-full pl-20 pr-8" :placeholder="t.messages.searchPlaceholder" @keyup.enter="queryMessages" />
+        <!-- 搜索范围选择器（绝对定位在输入框内左侧） -->
+        <select v-model="searchIn" class="absolute left-1 top-1/2 -translate-y-1/2 select select-ghost select-xs w-16 p-0 h-auto text-xs bg-transparent border-none focus:outline-none cursor-pointer text-base-content/70 hover:text-base-content">
+          <option value="all">{{ t.messages.searchInAll }}</option>
+          <option value="key">{{ t.messages.searchInKey }}</option>
+          <option value="value">{{ t.messages.searchInValue }}</option>
+        </select>
         <button v-if="searchKeyword" class="absolute right-2 top-1/2 -translate-y-1/2 text-base-content/40 hover:text-base-content" @click="searchKeyword = ''; queryMessages()">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
